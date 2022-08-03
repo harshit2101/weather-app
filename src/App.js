4760f290b76c140b97fb664089bacf78754b09ec
harshit2101 <harshitverma.2101.hv@gmail.com>
2022-08-03 23:12:45 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TopButton from "./components/TopButton";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import Weather from "./components/Weather";
+import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/weatherService";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
+const App=()=> {
+  const [query, setQuery] = useState({ q: "delhi" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const message = query.q ? query.q : "current location.";
+
+      toast.info("Fetching weather for " + message);
+
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        toast.success(
+          `Successfully fetched weather for ${data.name}, ${data.country}.`
+        );
+
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
+
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700";
+    const limit1 = units === "metric" ? 10 : 50;
+    const limit2 = units === "metric" ? 20 : 60;
+    if (weather.temp <= limit1) return "from-cyan-700 to-blue-700";
+    if(weather.temp>limit1 && weather.temp<=limit2) return " from-gray-400 to to-gray-900  "
+    return "from-yellow-700 to-orange-700 ";
+  };
+
+  const formatBackgroundImage = () => {
+    if (!weather) return "bg-[url('https://cdn.wallpapersafari.com/86/45/75RTke.jpg')]";
+    const limit1 = units === "metric" ? 10 : 50;
+    const limit2 = units === "metric" ? 20 : 60;
+    if (weather.temp <= limit1) return " bg-[url('http://www.itsjustlight.com/wp-content/uploads/2014/08/arctic-nunavut-35.jpg')]";
+    if(weather.temp>limit1 && weather.temp<=limit2) return " bg-[url('https://live.staticflickr.com/7456/10173336893_26b5aa21f3.jpg')]"
+    return " bg-[url('https://cdn.wallpapersafari.com/86/45/75RTke.jpg')]";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={`${formatBackgroundImage()} bg-no-repeat bg-cover h-fit bg-blend-darken `}>
+      <div className="backdrop-blur-sm">
+        <div
+          className={`mx-auto max-w-screen-md  py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
         >
-          Learn React
-        </a>
-      </header>
+          <TopButton setQuery={setQuery} />
+          <Inputs setQuery={setQuery} units={units} setUnits={setUnits}  />
+
+          {weather && (
+            <div>
+              <TimeAndLocation weather={weather} />
+              <Weather weather={weather} />
+
+              <Forecast title="hourly forecast" items={weather.hourly} />
+              <Forecast title="daily forecast" items={weather.daily} />
+            </div>
+          )}
+
+          <ToastContainer autoClose={2000} theme="colored" newestOnTop={true} />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// className={`${formatBackgroundImage()} bg-no-repeat bg-cover h-fit bg-blend-darken `}
+// className={`mx-auto max-w-screen-md  py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
---
+++ src/components/TopButton.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const TopButton = ({setQuery}) => {
+
+    const cities=[
+        {
+            id: 1,
+            title: 'Delhi'
+        },
+        {
+            id: 2,
+            title: 'Mumbai'
+        },
+        {
+            id: 3,
+            title: 'Kolkata'
+        },
+        {
+            id: 4,
+            title: 'Chennai'
+        },
+        {
+            id: 5,
+            title: 'Indore'
+        },
+    ]
+
+
+    return (
+        <div className="flex items-center justify-around my-6 ">
+          {cities.map((city) => (
+            <button
+              key={city.id}
+              className="text-white text-lg font-medium hover:scale-105"
+              onClick={() => setQuery({ q: city.title })}
+            >
+              {city.title}
+            </button>
+          ))}
+        </div>
+      );
+    }
+
+export default TopButton
